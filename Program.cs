05bcbe75d6a49fa02707d2b4133fa643d253652a
Saroeut Sim <saroeuts123@gmail.com>
2024-04-01 12:52:22 -0700

    edit

--- Program.cs
+++ Program.cs
@@ -16,35 +16,9 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-// app.MapGet("/weatherforecast", () =>
-// {
-//     var forecast =  Enumerable.Range(1, 5).Select(index =>
-//         new WeatherForecast
-//         (
-//             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-//             Random.Shared.Next(-20, 55),
-//             summaries[Random.Shared.Next(summaries.Length)]
-//         ))
-//         .ToArray();
-//     return forecast;
-// })
-// .WithName("GetWeatherForecast")
-// .WithOpenApi();
-
 app.MapGet("/hello", (string firstName, string LastName) => {
-    return "Hello " + firstName + " " + LastName;
+    return "Hello " + firstName + " " + LastName + "" + ", " + "nice to meet you!";
 
 });
 
-
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
--- obj/Debug/net8.0/SSimMiniChallenge1Endpoint.AssemblyInfo.cs
+++ obj/Debug/net8.0/SSimMiniChallenge1Endpoint.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SSimMiniChallenge1Endpoint")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28dbc5198b86a2e3d7a21a8fb0549882574d8dcd")]
 [assembly: System.Reflection.AssemblyProductAttribute("SSimMiniChallenge1Endpoint")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SSimMiniChallenge1Endpoint")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
